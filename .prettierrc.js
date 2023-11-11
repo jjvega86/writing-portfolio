8606fbe4cbf9319de683b2e927997436ee8634e5
JJ <jj.vega86@gmail.com>
2023-11-11 11:47:55 -0600

    Initial commit from create-remix

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	bracketSpacing: true,
+	bracketSameLine: true,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	jsxSingleQuote: false,
+	arrowParens: 'avoid',
+	printWidth: 120,
+	endOfLine: 'lf',
+	plugins: [require('prettier-plugin-tailwindcss')],
+};
---
+++ app/root.tsx
@@ -0,0 +1,27 @@
+import type { LinksFunction } from '@remix-run/node';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+
+import styles from './tailwind.css';
+import globalStyles from './styles/globals.css';
+
+export const links: LinksFunction = () => [
+	{ rel: 'stylesheet', href: styles },
+	{ rel: 'stylesheet', href: globalStyles },
+];
+
+export default function App() {
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
+}
